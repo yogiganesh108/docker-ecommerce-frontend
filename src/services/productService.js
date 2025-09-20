81899b7e4a1e85b3d18e090f1e600de73682d4cf
I.Y.G Nagendra <103441226+yogiganesh108@users.noreply.github.com>
2025-09-20 09:48:25 +0530

    Update productService.js

--- src/services/productService.js
+++ src/services/productService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_BASE_URL = "http://ec2-52-91-7-151.compute-1.amazonaws.com:8081/api/products";  // Backend API URL
+const API_BASE_URL = "http://54.160.233.105:8081/api/products";  // Backend API URL
 
 // Fetch products by category (computers, mobiles, laptops, pendrives, or all)
 export const getProducts = async (category = "") => {
